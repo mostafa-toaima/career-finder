ee5e363616e896024223fa87bb74b3ec0a9886ff
mostafa-toaima <moustafaebrahim124@gmail.com>
2025-03-31 17:01:07 +0200

    add prerendered routes and new chunk files for performance tracking and component initialization

--- docs/chunk-J4UH3DB3.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-QHYIGAQ6.js";import"./chunk-OL3OQDXW.js";import"./chunk-TF7BHFBT.js";import"./chunk-Z4O4EWBG.js";import"./chunk-OBTNBA2B.js";export{a as SignUpComponent};
---
+++ docs/chunk-UNI4JIBJ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-66COIVHF.js";import"./chunk-P5PUGBH6.js";import"./chunk-7E5E4JX3.js";import"./chunk-HF5DDPS3.js";import"./chunk-K65C4JJG.js";import"./chunk-YOCB2I3H.js";import"./chunk-KARPGTJI.js";import"./chunk-ACKELEN3.js";export{a as LoginComponent};
